cc74c6f15e85109892adcf0c1abadfc0ef30fda2
Guido Flohr <guido.flohr@cantanea.com>
2018-10-18 18:34:18 +0300

    more boilerplate

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,22 +1,30 @@
 'use strict';
 const Generator = require('yeoman-generator');
-const chalk = require('chalk');
+// Const chalk = require('chalk');
 const yosay = require('yosay');
+const pkg = require('../../package.json');
+// Const askName = require('inquirer-npm-name');
 
 module.exports = class extends Generator {
+  constructor(args, opts) {
+    super(args, opts);
+
+    this.installTasks = {
+      nmp: false,
+      bower: false
+    };
+  }
+
   prompting() {
-    // Have Yeoman greet the user.
-    this.log(
-      yosay(`Welcome to the mathematical ${chalk.red('generator-qgoda')} generator!`)
-    );
+    this.log(yosay(`Qgoda generator version ${pkg.version}`));
 
     const prompts = [
-      {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true
-      }
+      //            {
+      //                type: 'confirm',
+      //                name: 'someAnswer',
+      //                message: 'Would you like to enable this option?',
+      //                default: true
+      //            }
     ];
 
     return this.prompt(prompts).then(props => {
@@ -27,12 +35,13 @@ module.exports = class extends Generator {
 
   writing() {
     this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+      this.templatePath('favicon.png'),
+      this.destinationPath('images/favicon.png')
     );
+    this.fs.copy(this.templatePath('index.md'), this.destinationPath('index.md'));
   }
 
   install() {
-    this.installDependencies();
+    // This.installDependencies(this.installTasks);
   }
 };
---
+++ generators/app/templates/index.md
@@ -0,0 +1,7 @@
+---
+title: Homepage
+location: /index.html
+---
+## This site was generated with Qgoda
+
+Continue by writing real content.

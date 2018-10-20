a4af7aa183ac6b2ec81a67d6da3118c7d4fe4b83
Guido Flohr <guido.flohr@cantanea.com>
2018-10-20 10:57:46 +0300

    more tests

--- __tests__/app.js
+++ __tests__/app.js
@@ -11,7 +11,18 @@ describe('generator-qgoda:app', () => {
 	});
 
 	it('creates files', () => {
+		assert.file(['_config.yaml']);
+		assert.file(['package.json']);
 		assert.file(['index.md']);
-		assert.file(['index.md']);
+		assert.file(['favicon.ico']);
+		assert.file(['static/images/favicon.png']);
+		assert.file(['_views/default.html']);
+		assert.file(['_views/raw']);
+		assert.file(['_views/functions/setup.tt']);
+		assert.file(['_views/functions/scripts.tt']);
+		assert.file(['_views/functions/styles.tt']);
+		assert.file(['_views/partials/body.html']);
+		assert.file(['_views/partials/head.html']);
+		assert.file(['_views/wrappers/html5.html']);
 	});
 });
